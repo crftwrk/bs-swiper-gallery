2e518bb850a78b106e180887758c7ed679eefc18
craftwerk <pst@crftwrk.de>
2022-10-05 19:34:54 +0200

    Disable clickable pagination

--- main.php
+++ main.php
@@ -232,7 +232,7 @@ function wptsjg_render_gallery(string $block_content, array $block) {
       if ($is_pagination_enabled) {
          $swiper_config['pagination'] = array(
             'el' => sprintf('#%s .swiper-pagination', esc_attr($gallery_name)),
-            'clickable' => 'true',
+            //'clickable' => 'true',
          );
       }
 
